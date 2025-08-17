dbdb9e79167a255bea401d7a432765b7f998310c
KlavitoOfTech <ejibeclaver0@gmail.com>
2025-08-17 21:57:41 +0100

    repair: inserted new blocks

---
+++ src/components/data.js
@@ -0,0 +1,64 @@
+import Pic1 from '../images/team-pic-1.jpeg';
+import Pic2 from '../images/team-pic-2.jpeg';
+import Pic3 from '../images/team-pic-3.jpeg';
+import Pic4 from '../images/team-pic-4.jpeg';
+import Pic5 from '../images/team-pic-5.jpeg';
+import Pic6 from '../images/team-pic-6.jpeg';
+import Pic7 from '../images/team-pic-7.jpeg';
+import Pic8 from '../images/team-pic-8.jpeg';
+import Pic9 from '../images/team-pic-9.jpeg';
+import Pic10 from '../images/team-pic-10.jpeg';
+
+
+export const products = [
+  {
+    name: 'Kennedy Osigwe, ACA',
+    description: 'Senior Business Analyst, Venture Garden Group',
+    image: [Pic1]
+  },
+  {
+    name: 'Olajumoke Oyeyemi, ACA',
+    description: 'Career Consultant & Co-founder, Cv2essence',
+    image: [Pic2]
+  },
+  {
+    name: 'Tolu Enitilo, ACA, ACCA Affiliate',
+    description: 'Group Finance Analyst, Nordea Bank Abp, Estonia Office, Northern Europe',
+    image: [Pic3],
+  },
+  {
+    name: 'Esther Opadoyin, FMVA,',
+    description: 'Outreach Specialist & Project Manager, Velocity Company, Atlanta Georgia',
+    image: [Pic4]
+  },
+  {
+    name: 'Ifeoluwa Farayola',
+    description: 'Elegant and long-lasting fragrance.',
+    image: [Pic5]
+  },
+  {
+    name: 'Damilola Agbebi',
+    description: 'Financial Consultant, Leadway Assurance Plc',
+    image: [Pic6]
+  },
+  {
+    name: 'Emmanuella Ishola',
+    description: 'Trade Reconciliation Specialist, First Bank of Nigeria Ltd (Head Office)',
+    image: [Pic7]
+  },
+  {
+    name: 'Prof. R.O Salawu',
+    description: 'Strategic Development & Industry Partnerships',
+    image: [Pic8]
+  },
+  {
+    name: 'Dr James Monday',
+    description: 'Financial Research & Innovation',
+    image: [Pic9]
+  },
+  {
+    name: 'Dr Q.A Lawal',
+    description: 'Student Development & Curriculum Alignment',
+    image: [Pic10]
+  },
+];
